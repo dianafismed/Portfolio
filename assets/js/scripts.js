b5e1b30f26d69d7c3977ba0a71e433fa9e43e53d
Diana Avila <45690382+dianafismed@users.noreply.github.com>
2025-03-17 17:39:16 -0300

    up

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -12,7 +12,7 @@ myModal.addEventListener("shown.bs.modal", () => {
   myInput.focus();
 });
 
-function whatsapp() {
-  let send = document.querySelector("#whatsapp");
-  send.setAttribute("href", "https://wa.me/5516981994052");
+function change() {
+  document.body.classList.toggle("dark-theme");
+  document.body.classList.toggle("light-theme");
 }
